53768e46f9b870b69758bb9447f1b7c65aa09ff8
Víctor M. Chaves García <victormcg96@gmail.com>
2023-01-31 00:10:13 +0100

    Add controller and routes to users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < AuthorizedController
+  
+  def my_data
+
+  end
+
+  def user_data
+
+  end
+
+  def list_from_group
+    
+  end
+
+  def add_user_to_group
+
+  end
+
+  def delete_from_group
+
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,16 @@ Rails.application.routes.draw do
       get 'managed_groups', to: 'groups#managed_groups'
     end
   end
+
+  resources :users, except: [:show, :update, :destroy] do
+    collection do 
+      get 'my_data', to: 'users#my_data'
+      get 'from_group/:group', to: 'users#list_from_group'
+    end
+    member do
+      get '', to: 'users#user_data'
+      put 'add_to_group/:group', to: 'users#add_user_to_group'
+      delete 'delete_from_group/:group', to: 'users#delete_from_group' 
+    end
+  end
 end
